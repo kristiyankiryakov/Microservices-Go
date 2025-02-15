d972e45f3ce8b0dda9eb94782073e7c252cec156
Kris <emineo@abv.bg>
2025-02-15 14:20:27 +0200

    add logging via rabbitMq

--- listener-service/main.go
+++ listener-service/main.go
@@ -43,7 +43,7 @@ func connect() (*amqp.Connection, error) {
 
 	// don't continue until rabbit is ready
 	for {
-		c, err := amqp.Dial("amqp://guest:guest@rabbitmq")
+		c, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
 		if err != nil {
 			fmt.Println("RabbitMQ not yet ready...")
 			counts++
